2f35baf421e2878c214c56c0857aa57a5d0f3e88
TinyKay <tinykay@126.com>
2018-07-09 17:09:40 +0800

    +

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,10 +1,10 @@
 import datetime
 
 
-begin = datetime.date(2018, 1, 1)
-end = datetime.date(2018, 6, 13)
+begin = datetime.date(2018, 6, 1)
+end = datetime.date(2018, 7, 6)
 for i in range((end - begin).days+1):
     day = begin + datetime.timedelta(days=i)
-    print("union SELECT '{days}' as day,count(distinct member_id) as amount  FROM `order` "
-          "WHERE `status` =1 and `gmt_created` <date_add('{days}',interval +1 day) and `gmt_created` >='{days}' "
+    print("union SELECT '{days}' as day,count(distinct member_id) ,sum(invest_amount)  FROM `order` "
+          "WHERE `status` =1 and `gmt_created` <date_add('{days}',interval +1 day) and `end_date` >'{days}' "
           .format(days=str(day)))
